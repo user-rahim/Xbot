11ffc1c74ece3b3257a6e10bacf45880bcc8a1e8
user-rahim <35132717+user-rahim@users.noreply.github.com>
2019-09-29 08:08:55 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 green='\e[1;32m'
 red='\e[1;31m'
-#Header
-ulang='y'
-while [ $ulang == 'y' ]
-do
 echo "Please Wait... "
 sleep 2
 pkg install unzip -y
@@ -19,8 +15,3 @@ echo "Please Wait... "
 sleep 2
 echo -e "${red}Done Install Module Now Runing Comand python2.py To Run AutoExploit\n"
 ls
-fi
-echo -n -e "\e[1;31mNext Or Exit? (Select y/n): "
-read ulang
-done
-clear
