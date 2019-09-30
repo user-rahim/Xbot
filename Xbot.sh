6c520d754bfc2a0d1f7fe4d52b18e20955c8fa43
user-rahim <35132717+user-rahim@users.noreply.github.com>
2019-09-30 09:46:07 -0700

    Create Xbot.sh

---
+++ Xbot.sh
@@ -0,0 +1,118 @@
+#!/system/xbin/bash
+clear
+toilet -f slant --gay "Xbot Tools"
+sleep 1
+echo "\033[31;1m Website Vulnerability Scanner Grabber And Auto Exploiter"
+sleep 1
+echo "\033[33;1m Author: X-Mr.R4h1M"
+sleep 1
+echo "\033[32;1m ★contact: user.rahim@gmail.com"
+sleep 1
+echo "\033[34;1m ★AndroSec1337 Cyber Team"
+sleep 1
+echo "1. Priv Tools Grabber & Mass Add http://"
+echo "2. Priv AutoExploiter V1.0"
+echo "3. Priv AutoExploiter V2.0"
+echo "4. Priv ICGAutoExploiter V2.5"
+echo "5. Priv SMTP Cracker"
+echo "6. Priv Reverse ip"
+echo "7. Priv cPanel Detect And Brute"
+echo "8. Priv Dorker"
+echo "9. Install All Module & Package Xbot"
+echo "0. Keluar"
+echo "\033[30;1m00. This Tool Information"
+echo "\033[33;1m Select Use > "
+read mrrm
+if [ $mrrm = 1 ] || [ $mrrm = 1 ]
+then
+clear
+python2 priv8.py
+fi
+
+if
+[ $mrrm = 2 ] || [ $mrrm = 2 ]
+then
+clear
+perl v1.pl
+fi
+
+if [ $mrrm = 3 ] || [ $mrrm = 3 ]
+then
+clear
+perl v1.pl -u site.txt -t 16
+fi
+
+
+if [ $mrrm = 4 ] || [ $mrrm = 4 ]
+then
+clear
+python2 ICG.py 3
+fi
+
+if
+[ $mrrm = 5 ] || [ $mrrm = 5 ]
+then
+clear
+python2 smtp.py
+fi
+
+if
+[ $mrrm = 6 ] || [ $mrrm = 6 ]
+then
+python2 ips.py
+fi
+
+if
+[ $mrrm = 7 ] || [ $mrrm = 7 ]
+then
+python2 cpanel.py 50 list.txt passcpanel.txt
+fi
+
+if
+[ $mrrm = 8 ] || [ $mrrm = 8 ]
+then
+perl dork.pl
+fi
+
+if
+[ $mrrm = 9 ] || [ $mrrm = 9 ]
+then
+bash instal.sh
+fi
+
+if
+[ $mrrm = 00 ] || [ $mrrm = 00 ]
+then
+clear
+toilet -f slant --gay "Xbot Tools"
+echo "\033[31;1mNama tools: BomBer Mux"
+sleep 1
+echo "\033[33;1mAuthor: X-Mr.R4h1M"
+sleep 1
+echo "\033[32;1mVersion: 1.0"
+sleep 1
+echo "\033[36;1mTeam: ★Androsec1337 Cyber Team★"
+sleep 1
+echo "\033[34;1mMore Information Visit: https://www.github.com/user-rahim/"
+echo "\033[31;1m Spesial Thanks To: "
+echo "\033[36;1m"
+echo "./Xi4u7i"
+echo "N07_4L0N3"
+echo "Mr_/Boon'007"
+echo "-xGans"
+echo "X-Mr.R4h1M"
+sleep 1
+echo "And All Member AndroSec1337 Cyber Team"
+echo "\033[30;1m Please Wait 8 Seconds"
+sleep 8
+sh Xbot.sh
+fi
+
+if
+[ $mrrm = 0 ] || [ $mrrm = 0 ]
+then
+echo "\033[31;1m Sing Out"
+sleep 1
+echo "\033[32;1m Thanks :)"
+sleep 1
+fi
